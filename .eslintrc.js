be20dfe95163ff097b6246572be6bdc43eb01bfb
Denis Arlakov <darlakov@mail.ru>
2024-04-21 14:02:17 +0300

    18 Настраиваем EsLint. Исправляем ошибки

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,18 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb'],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: { jsx: true },
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint'],
+    plugins: ['react', '@typescript-eslint', 'i18next'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -33,6 +37,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'object-curly-newline': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: { __IS_DEV__: true },
 };
