c8eff8a380bb9d3945d880eeb7fd1dfca86b389d
Denis Arlakov <darlakov@mail.ru>
2024-05-23 00:20:29 +0300

    add profile, optimization render

---
+++ src/shared/const/common.ts
@@ -0,0 +1,15 @@
+export enum Currency {
+    'RUB' = 'RUB',
+    'USD' = 'USD',
+    'EUR' = 'EUR',
+}
+
+export enum Country {
+    Russia = 'Russia',
+    Ukraine = 'Ukraine',
+    Belarus = 'Belarus',
+    Germany = 'Germany',
+    France = 'France',
+    UK = 'UK',
+    USA = 'USA',
+}
